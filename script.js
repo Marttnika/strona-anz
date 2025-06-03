3c025f088e5205883dc1d514c449128b8bdfad30
Marttnika <ye02@op.pl>
2025-06-03 10:40:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
     message: document.querySelector('textarea[name="message"]').value
   };
 
-  // Wysyłka danych do Azure Function (zapis do Table Storage)
+  // Wysyłka do Azure Function
   fetch("https://submitform-api.azurewebsites.net/api/submitForm?code=nNq8JE9yb4MFDorPYAPaBta9XFfu7wMiRxNZfVx_bV7DAzFu_ssLIg==", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -17,7 +17,7 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
     .then(data => {
       alert("Dziękujemy! Twoja wiadomość została wysłana.");
 
-      // Wysyłka e-maila przez EmailJS v2
+      // EmailJS v4 – wywołanie wysyłki maila
       emailjs.send("service_om6pfoz", "template_knk9bj9", formData)
         .then(function (response) {
           console.log("E-mail wysłany!", response.status, response.text);
