9ffa9b39e580742fb54841d159335a9e46c1ebf6
Marttnika <ye02@op.pl>
2025-06-03 09:06:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 document.getElementById("contactForm").addEventListener("submit", function (e) {
   e.preventDefault();
-  document.getElementById("status").textContent = "Wysyłanie...";
-  // Tu w kolejnych krokach dodamy wysyłkę danych
+
+  const formData = {
+    name: document.querySelector('input[name="name"]').value,
+    email: document.querySelector('input[name="email"]').value,
+    message: document.querySelector('textarea[name="message"]').value
+  };
+
+  fetch("https://submitform-api.azurewebsites.net/api/submitForm?code=nNq8JE9yb4MFDorPYAPaBta9XFfu7wMiRxNZfVx_bV7DAzFu_ssLIg==", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData)
+  })
+    .then(response => response.json())
+    .then(data => {
+      alert("Dziękujemy! Twoja wiadomość została wysłana.");
+    })
+    .catch(error => {
+      console.error("Błąd:", error);
+      alert("Wystąpił problem z wysyłką formularza.");
+    });
 });
