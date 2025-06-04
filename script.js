d9afc6537c317913a5a3dd9a89fd6a934636f6e5
Marttnika <ye02@op.pl>
2025-06-04 11:52:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.getElementById("contactForm").addEventListener("submit", function (e) {
   e.preventDefault();
+  if (window.appInsights) {
+  appInsights.trackEvent({ name: "submit_click" });
+}
+
 
   const name = document.querySelector('input[name="name"]').value.trim();
   const email = document.querySelector('input[name="email"]').value.trim();
